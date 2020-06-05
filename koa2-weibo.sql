e03838753a50a7e7069af8aa62af607a7196b5d9
李亚军 <dreamoflu@163.com>
2020-06-05 22:42:27 +0800

    feat: 新增数据库演示命令

---
+++ koa2-weibo.sql
@@ -0,0 +1,15 @@
+show databases;
+use koa2_weibo_db;
+select * from users;
+select username,nickname from users where username='zhangsan'and`password` = '123';
+insert into users (username,`password`,nickname) values ('lisi','123','李四');
+insert into blogs (title,content,userid) values ('标题5','内容5','2');
+
+update blogs set content = '内容1内容1' where id = '1';
+delete from blogs where id = 3;
+select * from blogs order by id desc; -- 倒序排列
+select count(*) as `count` from blogs;
+select count(id) as `count` from blogs; -- 查询数量
+
+select * from blogs order by id desc limit 2; -- 分页查询
+select * from blogs order by id desc limit 2 offset 2; -- 分页查询 跳过两条也就是查询第二页的数据
